2e30fac87862a74715a365ef5674546a4a5b9f87
Seren_Modz 21 <seren@kings-world.net>
2024-10-23 10:22:41 +0100

    refactor: replace pg with postgres.js
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,5 +1,5 @@
 import { envParseString } from "@skyra/env-utilities";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema.js";
 
 export const db = drizzle(envParseString("DATABASE_URL"), { schema });
