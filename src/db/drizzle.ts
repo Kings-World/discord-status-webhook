cced647048bdcff76b766c726c02545f7a515af4
Seren_Modz 21 <seren@kings-world.net>
2024-10-24 23:53:54 +0100

    revert: go back to using pg instead of postgres.js
    currently getting the following:
    PostgresError: unrecognized configuration parameter "schema"
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,5 +1,5 @@
 import { envParseString } from "@skyra/env-utilities";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema.js";
 
 export const db = drizzle(envParseString("DATABASE_URL"), { schema });
