8d65a55fb23278088a8c8179d2861e0e40da56f6
Seren_Modz 21 <seren@kings-world.net>
2025-12-01 01:03:39 +0000

    style: indent with tabs instead of spaces (#86)

--- src/constants.ts
+++ src/constants.ts
@@ -5,28 +5,28 @@ import { WebhookClient } from "discord.js";
 import type { IncidentStatusEnum } from "./zod.js";
 
 export const incidentsJsonUrl =
-    "https://discordstatus.com/api/v2/incidents.json";
+	"https://discordstatus.com/api/v2/incidents.json";
 
 export const logger = new Logger({ level: LogLevel.Debug });
 
 export const statusData: Record<
-    IncidentStatusEnum,
-    [color: number, emoji: string]
+	IncidentStatusEnum,
+	[color: number, emoji: string]
 > = {
-    identified: [0xf15832, envParseString("IDENTIFIED_STATUS_EMOJI")],
-    investigating: [0xed9932, envParseString("INVESTIGATING_STATUS_EMOJI")],
-    monitoring: [0xf2ef42, envParseString("MONITORING_STATUS_EMOJI")],
-    resolved: [0x43b582, envParseString("RESOLVED_STATUS_EMOJI")],
-    postmortem: [0x43b582, envParseString("RESOLVED_STATUS_EMOJI")],
+	identified: [0xf15832, envParseString("IDENTIFIED_STATUS_EMOJI")],
+	investigating: [0xed9932, envParseString("INVESTIGATING_STATUS_EMOJI")],
+	monitoring: [0xf2ef42, envParseString("MONITORING_STATUS_EMOJI")],
+	resolved: [0x43b582, envParseString("RESOLVED_STATUS_EMOJI")],
+	postmortem: [0x43b582, envParseString("RESOLVED_STATUS_EMOJI")],
 };
 
 export const webhook = envIsDefined("WEBHOOK_ID", "WEBHOOK_TOKEN")
-    ? new WebhookClient({
-          id: envParseString("WEBHOOK_ID"),
-          token: envParseString("WEBHOOK_TOKEN"),
-      })
-    : null;
+	? new WebhookClient({
+			id: envParseString("WEBHOOK_ID"),
+			token: envParseString("WEBHOOK_TOKEN"),
+		})
+	: null;
 
 export const roleId = envIsDefined("ROLE_ID")
-    ? envParseString("ROLE_ID")
-    : null;
+	? envParseString("ROLE_ID")
+	: null;
