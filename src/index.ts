7d156c2df6ab69a972e0b7e012298dba4d87b7b5
Seren_Modz 21 <seren@kings-world.net>
2025-12-21 22:38:47 +0000

    feat: add dev tools
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "@skyra/env-utilities/setup";
+import { DevTools } from "@effect/experimental/index";
 import { NodeRuntime } from "@effect/platform-node";
 import { Effect, Layer } from "effect";
 import { CronJobService } from "./cron.js";
@@ -6,7 +7,11 @@ import * as Drizzle from "./db/drizzle.js";
 import { ServerLive } from "./http.js";
 import * as Webhook from "./webhook.js";
 
-export const MainLive = Layer.mergeAll(Drizzle.fromEnv, Webhook.fromEnv);
+export const MainLive = Layer.mergeAll(
+	Drizzle.fromEnv,
+	Webhook.fromEnv,
+	DevTools.layer(),
+);
 
 const Services = Layer.mergeAll(CronJobService, ServerLive);
 
