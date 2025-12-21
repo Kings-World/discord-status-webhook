2056e82b97f926384cfd094e0d95a5555468239e
Seren_Modz 21 <seren@kings-world.net>
2025-12-21 22:38:02 +0000

    refactor(http): switch to json responses
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/http.ts
+++ src/http.ts
@@ -9,22 +9,26 @@ import {
 } from "@effect/platform";
 import { NodeHttpServer } from "@effect/platform-node";
 import { Effect, Layer, Schema } from "effect";
-import { processIncident } from "./process";
-import { WebhookPayloadSchema } from "./schema";
+import { processIncident } from "./process.js";
+import { WebhookPayloadSchema } from "./schema.js";
+
+const SuccessStruct = Schema.Struct({ message: Schema.String });
 
 const Api = HttpApi.make("Webhook").add(
 	HttpApiGroup.make("Routes")
-		.add(HttpApiEndpoint.get("route")`/`.addSuccess(Schema.String))
+		.add(HttpApiEndpoint.get("route")`/`.addSuccess(SuccessStruct))
 		.add(
 			HttpApiEndpoint.post("webhook")`/`
 				.setPayload(WebhookPayloadSchema)
-				.addSuccess(Schema.String),
+				.addSuccess(SuccessStruct),
 		),
 );
 
 const RoutesLive = HttpApiBuilder.group(Api, "Routes", (handlers) =>
 	handlers
-		.handle("route", () => Effect.succeed("Nothing to see here."))
+		.handle("route", () =>
+			Effect.succeed({ message: "Nothing to see here." }),
+		)
 		.handle("webhook", ({ payload }) =>
 			Effect.gen(function* () {
 				if ("component" in payload) {
@@ -34,14 +38,16 @@ const RoutesLive = HttpApiBuilder.group(Api, "Routes", (handlers) =>
 					yield* Effect.logInfo(
 						`The ${name} (${id}) ${component_type} updated from ${old_status} to ${new_status}`,
 					);
-					return yield* Effect.succeed(
-						"The component update has been processed",
-					);
+					return yield* Effect.succeed({
+						message: "The component update has been processed",
+					});
 				}
 
 				yield* processIncident(payload.incident);
 
-				return yield* Effect.succeed("The incident has been processed");
+				return yield* Effect.succeed({
+					message: "The incident has been processed",
+				});
 			}).pipe(
 				Effect.catchAll((error) => Effect.fail(error)),
 				Effect.orDieWith((error) => Effect.logError(error)),
