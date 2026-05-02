2ca31e9594f32424f1a3adec6e4045dde9df0f2d
Seren_Modz 21 <seren@kings-world.net>
2026-05-02 20:51:42 +0100

    feat: allow hiding cron tick logs

--- src/cron.ts
+++ src/cron.ts
@@ -1,9 +1,8 @@
 import { checkApi } from "./lib/functions/checkApi";
 
 Bun.cron("*/5 * * * *", () => {
-	console.log("Cron job started ticking");
+	if (Bun.env["DEBUG"] === "true") console.log("Cron job started ticking");
 	void checkApi();
-	console.log("Cron job completed");
 });
 
 console.log("Cron job scheduled to run every 5 minutes");
