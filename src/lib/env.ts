55266708803479f00388fa61d02886801fdcd70d
Seren_Modz 21 <seren@kings-world.net>
2026-05-02 19:03:34 +0100

    refactor: stricter env var validation
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,14 +1,32 @@
 import { z } from "zod";
 
+const snowflakeSchema = z
+	.string()
+	.regex(/^\d{17,20}$/, "Snowflakes are 17-20 digits long");
+
 export const envSchema = z.object({
-	DATABASE_URL: z.string(),
-	WEBHOOK_ID: z.string(),
-	WEBHOOK_TOKEN: z.string(),
-	ROLE_ID: z.string().optional(),
-	IDENTIFIED_STATUS_EMOJI: z.string().optional(),
-	INVESTIGATING_STATUS_EMOJI: z.string().optional(),
-	MONITORING_STATUS_EMOJI: z.string().optional(),
-	RESOLVED_STATUS_EMOJI: z.string().optional(),
+	DATABASE_URL: z.url({
+		protocol: /^postgresql$/,
+		message: "Only PostgreSQL databases are supported",
+	}),
+	WEBHOOK_ID: snowflakeSchema,
+	WEBHOOK_TOKEN: z.string().regex(/^[\w-]+$/),
+	ROLE_ID: optional(snowflakeSchema),
+	IDENTIFIED_STATUS_EMOJI: optional(snowflakeSchema),
+	INVESTIGATING_STATUS_EMOJI: optional(snowflakeSchema),
+	MONITORING_STATUS_EMOJI: optional(snowflakeSchema),
+	RESOLVED_STATUS_EMOJI: optional(snowflakeSchema),
 });
 
-export const env = envSchema.parse(process.env);
+const parsed = envSchema.safeParse(process.env);
+if (!parsed.success) {
+	console.error("Environment variable validation failed: ");
+	console.log(z.prettifyError(parsed.error));
+	process.exit(1);
+}
+
+export const env = parsed.data;
+
+function optional<Output>(type: z.ZodType<Output>) {
+	return z.preprocess((val) => val || undefined, type.optional());
+}
